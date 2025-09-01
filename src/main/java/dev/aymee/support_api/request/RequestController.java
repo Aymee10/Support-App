53a6d2bc6e3435577bbd0e00c3155decc4e6ad5b
Aymee10 <areinar31@gmail.com>
2025-09-01 12:30:12 +0200

    feat/Change request status

--- src/main/java/dev/aymee/support_api/request/RequestController.java
+++ src/main/java/dev/aymee/support_api/request/RequestController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,15 +22,22 @@ public class RequestController {
    public RequestController(RequestService requestService) {
     this.requestService = requestService;
    }
+
      @GetMapping
     public ResponseEntity<List<RequestDto>> getAllRequests() {
         List<RequestDto> requests = requestService.getAllRequests();
         return ResponseEntity.ok(requests);
     }
+
     @PostMapping
     public ResponseEntity<RequestDto> createRequest(@Valid @RequestBody RequestCreationDto requestCreationDto) {
         RequestDto createdRequest = requestService.createRequest(requestCreationDto);
         return new ResponseEntity<>(createdRequest, HttpStatus.CREATED);
     }
  
+   @PatchMapping("/{id}")
+    public ResponseEntity<RequestDto> updateRequestStatus(@PathVariable Long id, @Valid @RequestBody RequestUpdateDto requestUpdateDto) {
+        RequestDto updatedRequest = requestService.updateRequestStatus(id, requestUpdateDto);
+        return ResponseEntity.ok(updatedRequest);
+    }
 }
